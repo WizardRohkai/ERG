d8734ae92386d6d90408c5718620822d8ebb95d8
WizardRohkai <wizardrokai@gmail.com>
2016-09-24 15:57:42 +0200

    Build 1.1
    Na saterdag se gestoei

---
+++ www/js/index.js
@@ -0,0 +1,11 @@
+document.getElementById("storePersonalInfo").addEventListener("click", setLocalStorage);
+var debugon = false;
+var localStorage = window.localStorage;
+function setLocalStorage() {
+      if(debugon) alert("test");
+      localStorage.setItem("Email", document.getElementById("eml").value);
+      localStorage.setItem("Username", document.getElementById("uname").value);
+      localStorage.setItem("Password", document.getElementById("psw").value);
+      localStorage.setItem("Debug", debugon);
+      window.location = "./login.html";
+  };
